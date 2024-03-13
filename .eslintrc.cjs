2e08d3bc94d286591efe572b868f6bea6af79f71
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-13 12:23:11 +0900

    chore: eslint settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2023: true,
+    node: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['plugin:prettier/recommended'],
+}
